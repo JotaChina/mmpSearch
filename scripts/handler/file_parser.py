323ed15f47810a01a5b4b0d72cb34e7fcdc0c623
JotaChina <72946706+JotaChina@users.noreply.github.com>
2025-05-13 20:43:45 -0300

    Update file_parser.py

--- scripts/handler/file_parser.py
+++ scripts/handler/file_parser.py
@@ -5,9 +5,12 @@ import importlib
 from automations.automations import parse_automation
 from basslines.basslines import parse_basslines
 from samples.samples import parse_samples
-from utils import SUPPORTED_PLUGINS
+from utils import SUPPORTED_PLUGINS #lembrar de inserir sempre que tiver um novo plugin
 
 # Lista dos plugins suportados
+
+import os
+import xml.etree.ElementTree as ET
 
 def parse_mmp_file(file_path):
     if not os.path.exists(file_path):
@@ -21,20 +24,33 @@ def parse_mmp_file(file_path):
         bpm = root.find('./head').attrib.get('bpm', 'N/A')
         tracks = root.findall('./song/trackcontainer/track')
         track_info = []
-        tags = {}
-        tags['TAG'] = []
-        tags['plugin'] = []
-        tags['sample'] = []
-        tags['bassline'] = []
-        tags['automation'] = []
+        tags = {
+            'TAG': [],
+            'plugin': [],
+            'sample': [],
+            'bassline': [],
+            'automation': []
+        }
+
+        projeto0_status = None
+        nova_versao = None
+
+        # Verificando e ajustando o campo projeto0
+        projeto0_tag = root.find('.//projeto0')
+        if projeto0_tag is None:
+            # Caso não tenha a tag projeto0, adiciona o caminho até o arquivo
+            projeto0_status = file_path
+            nova_versao = "1.0"  # Primeira versão do projeto
+        else:
+            # Caso tenha a tag, atualize a versão do projeto
+            projeto0_status = projeto0_tag.text
+            nova_versao = str(float(root.attrib.get("version", "0.0")) + 0.1)
 
         for track in tracks:
             track_name = track.attrib.get('name', 'N/A')
             track_type = track.attrib.get('type')
 
-            if track_type == '0':
-                # Faixas de instrumento
-                # Tem que verificar o sf2, verificar caminhos dos samples.
+            if track_type == '0':  # Faixas de instrumento
                 instruments = track.findall('./instrumenttrack')
                 for instrumenttrack in instruments:
                     instrument = instrumenttrack.find('.//instrument')
@@ -43,47 +59,41 @@ def parse_mmp_file(file_path):
                         if plugin_name:
                             plugin_info = parse_plugin_track(instrumenttrack, plugin_name)
                             if plugin_info:
-                                # Adiciona metadados da faixa também
                                 plugin_info['track_name'] = track_name
                                 plugin_info['type'] = 'plugin'
                                 track_info.append(plugin_info)
-                                # Marcando os nomes dos plugins presentes
-                                if 'plugin' not in tags['TAG']:                                    
+                                if 'plugin' not in tags['TAG']:
                                     tags['TAG'].append('plugin')
-                                # Se algum plugin estiver com o nome diferente do original
-                                if track_name not in tags:
-                                    if (track_name not in SUPPORTED_PLUGINS and
-                                        plugin_name not in tags['plugin']):                         
-                                        tags['plugin'].append(plugin_name)                                    
+                                if track_name not in tags['plugin']:
+                                    tags['plugin'].append(plugin_name)
 
-            elif track_type == '1':  # Bassline (aparentemente, correto)
+            elif track_type == '1':  # Bassline
                 bassline_info = parse_basslines(track)
                 if bassline_info:
                     track_info.append(bassline_info)
                     if bassline_info['tags'] is not None:
-                        if 'bassline' not in tags['TAG']: #certo
+                        if 'bassline' not in tags['TAG']:
                             tags['TAG'].append('bassline')
-                        if track_name not in tags['TAG']: #certo
-                            tags['bassline'].append(track_name) 
-                        if 'audiofileprocessor' not in tags['plugin']: #certo
-                            tags['plugin'].append(bassline_info['tags'])                                                
+                        if track_name not in tags['bassline']:
+                            tags['bassline'].append(track_name)
+                        if 'audiofileprocessor' not in tags['plugin']:
+                            tags['plugin'].append(bassline_info['tags'])
                         if 'tripleoscillator' not in tags['plugin']:
                             if bassline_info['tags'] == 'tripleoscillator':
-                                tags['plugin'].append(bassline_info['tags'])  
-                            
-                                                                        
-            elif track_type == '2':  # Sample (aparentemente, correto)
+                                tags['plugin'].append(bassline_info['tags'])
+
+            elif track_type == '2':  # Sample
                 sample_info = parse_samples(file_path, track)
                 if sample_info:
                     track_info.append(sample_info)
-                    if 'sample' not in tags['TAG']:                    
+                    if 'sample' not in tags['TAG']:
                         tags['TAG'].append('sample')
                     sample_name = sample_info['sample_name']
-                    if sample_name == None:
+                    if sample_name is None:
                         sample_name = track_name
                     tags['sample'].append(sample_name)
 
-            elif track_type in ['5', '6']:  # Automation (aparentemente, correto)
+            elif track_type in ['5', '6']:  # Automation
                 automation_info = parse_automation(file_path)
                 if automation_info:
                     track_info.append(automation_info)
@@ -92,9 +102,9 @@ def parse_mmp_file(file_path):
                     tags['automation'].append(track_name)
 
             else:
-                print('\n\n\n\n\n\nERRO\n\n\n\n\n\n')
-                return                   
-        
+                print(f"Tipo de track desconhecido: {track_type}")
+                return None
+
         file_name_with_extension = os.path.basename(file_path)
         file_name, _ = os.path.splitext(file_name_with_extension)
 
@@ -103,7 +113,9 @@ def parse_mmp_file(file_path):
             'src': file_path,
             'bpm': bpm,
             'tags': tags,
-            'tracks': track_info
+            'tracks': track_info,
+            'projeto0': projeto0_status,
+            'versao_projeto': nova_versao
         }
 
     except ET.ParseError as e:
